6f773fff95e79784f33c19451fc59986214e269c
Julia Woeste <julia.woeste@quinnipiac.edu>
2025-11-12 02:08:42 +0000

    Initial commit, all data files in data folder

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ df = pd.DataFrame({
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-df2 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+df2 = pd.read_csv('data/data.csv')
 
 fig2 = px.scatter(df2, x="gdp per capita", y="life expectancy",
                  size="population", color="continent", hover_name="country",
                  log_x=True, size_max=60)
 
 app.layout = html.Div([
-    html.H1('Hello Dash'),
+    html.H1('Hello'),
 
     html.Div('''
         Dash: A web application framework for your data. 

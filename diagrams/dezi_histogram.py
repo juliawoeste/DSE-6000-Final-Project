048b052c0488ba6b91265d0637a7be7abb08e999
deziharris6-cloud <dezi.harris6@gmail.com>
2025-11-20 19:37:03 -0500

    Add Dezi's histogram and choropleth scripts

---
+++ diagrams/dezi_histogram.py
@@ -0,0 +1,15 @@
+import plotly.express as px 
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Data/cleaned_data.csv')
+
+prev_df = df[['year', 'focus_prevalence_num']] 
+fig2 = px.histogram(data_frame=prev_df, x='focus_prevalence_num', facet_col='year', 
+                    nbins=20, labels={'focus_prevalence_num': 'Smoker %'}, facet_col_wrap=7,
+                    facet_col_spacing=.03, facet_row_spacing=.05,
+                    title='Percentage of all Smokers by Year') 
+fig2.update_layout(bargap=.05)
+fig2.update_xaxes(tickangle=45) 
+fig2.show()
